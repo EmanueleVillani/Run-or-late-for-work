95465cbbfe4c195250f2493365c57f21557dd304
Emanuele Villani <manu.lebon@hotmail.it>
2022-02-28 11:53:14 +0100

    Jump
    added gravity for jumping and a boolean value for jumping once when it hits the ground

---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isGrond = true;
+
+    void Start()
+    {  
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)&& isGrond)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrond = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrond = true;
+    }
+}
