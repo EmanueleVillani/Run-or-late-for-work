81698faaa826fc824ab5b29ce138f51a1ae6a547
Emanuele Villani <manu.lebon@hotmail.it>
2022-03-05 17:22:48 +0100

    particles
    particles and text game Over

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public ParticleSystem ParticlePlayer;
+    public ParticleSystem explosionePlayer;
     private Animator playerAnim;
     private Rigidbody playerRb;
     public float jumpForce = 10;
@@ -17,6 +19,7 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+       
         playerAnim = GetComponent<Animator>();
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
@@ -37,20 +40,23 @@ public class PlayerController : MonoBehaviour
             
             playerRb.AddForce(Vector3.up * jumpForce , ForceMode.Impulse);
             isGrond = false;
+            ParticlePlayer.Stop();
         }
 
       
     }
     private void OnCollisionEnter(Collision collision)
     {
-
+        ParticlePlayer.Stop();
         
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrond = true;
+            ParticlePlayer.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle")){
             gameOver = true;
+            explosionePlayer.Play();
             Debug.Log("GameOver");
             playerAnim.SetBool("Death_b", true);
             button.SetActive(true);
